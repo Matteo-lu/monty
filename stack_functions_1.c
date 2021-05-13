4cac03d2512db79f4ac45a6ad7f77bb591aba211
Yaeluribe <2962@holbertonschool.com>
2021-05-13 15:55:54 -0500

    corrected pall, added pint, pop & swap

--- stack_functions_1.c
+++ stack_functions_1.c
@@ -34,10 +34,84 @@ void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
 	stack_t *copy = NULL;
 
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
 	copy = *stack;
 	while (copy != NULL)
 	{
 		printf("%d\n", copy->n);
 		copy = copy->next;
+	}
+}
+
+/**
+ * pint - Function to print the value at the top of a stack
+ * @stack: Pointer to top
+ * @line_number: current line
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		dprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+/**
+ * pop -Removes the top element of a stack
+ * @stack: Pointer to the firts element of our stack
+ * @line_number: Instruction line number
+ * Return: void
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *copy = NULL;
+
+	if (*stack == NULL)
+	{
+		dprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->next == NULL)
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+	else
+	{
+		copy = *stack;
+		copy = copy->next;
+		copy->prev = NULL;
+		free(*stack);
+		*stack = copy;
+	}
+
+
+}
+/**
+ * swap - swaps the top and second elements of a stack
+ * @stack: Pointer to the firts element of our stack
+ * @line_number: Instruction line number
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *copy = NULL;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		copy = (*stack)->next;
+		(*stack)->next = copy->next;
+		(*stack)->prev = copy;
+		copy->prev = NULL;
+		copy->next = *stack;
+		*stack = copy;
 	}
 }
