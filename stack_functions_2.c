a099c54a41ae484ba404a95a40e5bec2b6cbc656
Yaeluribe <2962@holbertonschool.com>
2021-05-13 16:20:52 -0500

    New functions, add and nop

---
+++ stack_functions_2.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * add - adds the n value of two top elements in stack
+ * @stack: Pointer to top of the stack
+ * @line_number: Instruction line number
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t copy = NULL;
+
+	copy = *stack;
+	if (copy->next == NULL)
+	{
+		dprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	copy->next->n += (*stack)->n;
+	*stack = copy->next;
+	(*stack)->prev = NULL;
+	free(copy);
+}
+
+/**
+ * nop - Do nothing
+ * @stack: Pointer to top of the stack
+ * @line_number: Instruction line number
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
