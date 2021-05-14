3292c63154bc65dca53be5f235ea87f081368db0
Yaeluribe <2962@holbertonschool.com>
2021-05-14 13:04:06 -0500

    Changed prototypes

--- monty.h
+++ monty.h
@@ -39,7 +39,7 @@ typedef struct instruction_s
 } instruction_t;
 
 char **buff_separator(char *str, char *identificator);
-int get_func(char *s, stack_t **stack, unsigned int line_number);
+void get_func(char *s, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
